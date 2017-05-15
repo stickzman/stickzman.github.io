9a96ccc6a60e501e8b78e3fe822e4c8a19527795
stickzman <Daniel.ahl1@marist.edu>
2017-05-15 12:08:16 -0400

    fixed connection issue

--- jspeer/JSPeer.js
+++ jspeer/JSPeer.js
@@ -1,29 +1,27 @@
 var conn;
-var call;
+var connected = false;
 
-var peer = new Peer({key: 'pz37ds8uryrjm7vi', "secure": false, "debug": 2});
+var peer = new Peer({key: 'pz37ds8uryrjm7vi', "debug": 2});
 
 peer.on('open', function(id) {
   document.getElementById("id").innerHTML = 'My peer ID is: ' + id;
 });
 
 function connect() {
-	initConn(peer.connect(document.getElementById("input")));
+	var destID = document.getElementById("input").value;
+	initConn(peer.connect(destID));
 }
 
-peer.on('connection', function(c) {
-	initConn(c);
-});
+peer.on('connection', function(c) {initConn(c);});
 
 function initConn(c) {
 	conn = c;
 	conn.on('open', function() {
-	  // Receive messages
-	  conn.on('data', function(data) {
-		console.log('Received', data);
-	  });
-
-	  // Send messages
-	  conn.send('Hello!');
+		connected = true;
+		
+		conn.on('data', function(data) {
+			console.log('Received', data);
+		});	
 	});
+	alert("Connected to: " + conn.peer);
 }
