8208228f8831a984d7500dfa84ffdf0e21e55047
stickzman <Daniel.ahl1@marist.edu>
2017-05-15 11:18:27 -0400

    starting over

--- jspeer/JSPeer.js
+++ jspeer/JSPeer.js
@@ -3,6 +3,6 @@ var call;
 
 var peer = new Peer({key: 'pz37ds8uryrjm7vi', "secure": true});
 
-peer.on("open", function(id) {
-	document.getElementById("id").innerHTML = 'My peer ID is: ' + id;
+peer.on('open', function(id) {
+  console.log('My peer ID is: ' + id);
 });
