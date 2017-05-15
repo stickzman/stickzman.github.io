6c08f53564180f922421dece74427cf615269c41
stickzman <Daniel.ahl1@marist.edu>
2017-05-15 11:29:38 -0400

    making progress

--- jspeer/JSPeer.js
+++ jspeer/JSPeer.js
@@ -6,3 +6,21 @@ var peer = new Peer({key: 'pz37ds8uryrjm7vi', "secure": false, "debug": 3});
 peer.on('open', function(id) {
   console.log('My peer ID is: ' + id);
 });
+
+function connect() {
+	conn = peer.connect(document.getElementById("input"));
+}
+
+peer.on('connection', function(con) {
+	conn = con;
+});
+
+conn.on('open', function() {
+  // Receive messages
+  conn.on('data', function(data) {
+    console.log('Received', data);
+  });
+
+  // Send messages
+  conn.send('Hello!');
+});
