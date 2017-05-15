4ccbe32ae9d82cf1254c565c712a73b3cda752ab
stickzman <Daniel.ahl1@marist.edu>
2017-05-15 11:37:38 -0400

    fixed connn

--- jspeer/JSPeer.js
+++ jspeer/JSPeer.js
@@ -8,19 +8,23 @@ peer.on('open', function(id) {
 });
 
 function connect() {
-	conn = peer.connect(document.getElementById("input"));
+	initConn(peer.connect(document.getElementById("input")));
 }
 
-peer.on('connection', function(con) {
-	conn = con;
+peer.on('connection', function(c) {
+	initConn(c);
 });
 
-conn.on('open', function() {
-  // Receive messages
-  conn.on('data', function(data) {
-    console.log('Received', data);
-  });
+function initConn(c) {
+	conn = c;
+	conn.on('open', function() {
+	  // Receive messages
+	  conn.on('data', function(data) {
+		console.log('Received', data);
+	  });
 
-  // Send messages
-  conn.send('Hello!');
-});
+	  // Send messages
+	  conn.send('Hello!');
+	});
+}
+
