959351eaf9458905929c156cb0940f64e44e3bc2
stickzman <Daniel.ahl1@marist.edu>
2017-05-15 11:17:09 -0400

    starting over

--- jspeer/JSPeer.js
+++ jspeer/JSPeer.js
@@ -1,16 +1,5 @@
-var myStream;
-var otherStream;
 var conn;
 var call;
-
-p.then(function(mediaStream) {
-  var video = document.getElementById("myVid");
-  window.stream = mediaStream;
-  video.srcObject = mediaStream;
-  myStream = mediaStream;
-});
-
-p.catch(function(err) { console.log(err.name); });
 
 var peer = new Peer({key: 'pz37ds8uryrjm7vi', "secure": true});
 
