75d03786be279ab03a0dd8b9104f001a02deebb6
stickzman <Daniel.ahl1@marist.edu>
2017-05-15 11:23:43 -0400

    switched to unsecured conn

--- jspeer/JSPeer.js
+++ jspeer/JSPeer.js
@@ -1,7 +1,7 @@
 var conn;
 var call;
 
-var peer = new Peer({key: 'pz37ds8uryrjm7vi', "secure": true, "debug": 3});
+var peer = new Peer({key: 'pz37ds8uryrjm7vi', "secure": false, "debug": 3});
 
 peer.on('open', function(id) {
   console.log('My peer ID is: ' + id);
